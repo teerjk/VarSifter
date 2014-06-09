f5d61a42831d030036fae062c750314c06f2ff1a
Jamie K. Teer <Jamie.Teer@moffitt.org>
2014-06-09 10:18:26 -0400

    Version 1.7 -added JSON file for Ensembl Variant Effect Predictor output

--- VarSifter.java
+++ VarSifter.java
@@ -23,7 +23,7 @@ import components.TableSorter;
 */
 public class VarSifter extends JFrame implements ListSelectionListener, ActionListener, TableModelListener {
     
-    final static String version = "1.7BETA";
+    final static String version = "1.7";
     final static String id = "$Id$";
 
     final static int VARIANT_FILE = 0;
